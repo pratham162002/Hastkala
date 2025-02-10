7c65d42cd05f34f7d1713061c2cc72346cbce1a9
pratham sahu <prathamsahu877@gmail.com>
2025-02-11 01:01:26 +0530

    Fixed ALLOWED_HOSTS in settings.py

--- hastkala/settings.py
+++ hastkala/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-onjr=!whcblebkusr#bkwg71362ny3r&@@lmw!cb55)1835)=y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hastkala.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
